6b9874554c5b78cf7db22f7003b4962a3c338a04
Harel Panker <harelpanker@gmail.com>
2025-02-28 20:16:55 +0100

    Start with notion

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,2 @@
+// 15:52
+// https://www.youtube.com/watch?v=W41UjRGTVtk&t=108s&ab_channel=Siya
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import SectionHero from '@/app/components/home-page/section-hero';
+import { fetchPages } from '@/app/lib/notion';
 
-export default function Home() {
+export default async function Home() {
+	const posts = await fetchPages();
+
 	return (
 		<>
 			<SectionHero />
