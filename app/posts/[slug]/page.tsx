d340a3aa8daceb665ec469cab09a1b4dc455df80
Harel Panker <harelpanker@gmail.com>
2025-03-04 17:02:10 +0100

    adding comments

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -4,6 +4,9 @@ import { HygraphClient } from '@/app/lib/client';
 import { singlePost } from '@/app/lib/queries/single-post';
 import { Post } from '@/app/types/single-post';
 
+// https://github.com/hygraph/hygraph-nextjs-blog-starter/blob/main/src/app/posts/%5Bslug%5D/page.jsx
+// https://nextjs.org/docs/messages/sync-dynamic-apis
+
 async function getData(slug: string): Promise<Post> {
 	const client = HygraphClient();
 
