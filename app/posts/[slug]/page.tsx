15217b95aa81219752750e25c0dfb401d2f7f100
Harel Panker <harelpanker@gmail.com>
2025-03-05 20:32:29 +0100

    Adding post head

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,7 @@ import SectionHero from '@/app/components/post-page/section-hero';
 import { HygraphClient } from '@/app/lib/client';
 import { singlePost } from '@/app/lib/queries/single-post';
 import { Post } from '@/app/types/single-post';
+import { notFound } from 'next/navigation';
 
 // https://github.com/hygraph/hygraph-nextjs-blog-starter/blob/main/src/app/posts/%5Bslug%5D/page.jsx
 // https://nextjs.org/docs/messages/sync-dynamic-apis
@@ -14,6 +15,27 @@ async function getData(slug: string): Promise<Post> {
 
 	const { post }: { post: Post } = await client.request(singlePost, variables);
 	return post;
+}
+
+export async function generateMetadata(props: { params: Promise<{ slug: string }> }) {
+	const params = await props.params;
+	const post = await getData(params.slug);
+
+	if (!post) return notFound();
+
+	return {
+		title: post.seoTitle,
+		description: post.seoDescription,
+		openGraph: {
+			images: [
+				{
+					url: post.image.url,
+					width: post.image.width,
+					height: post.image.height,
+				},
+			],
+		},
+	};
 }
 
 export default async function Page(props: { params: Promise<{ slug: string }> }) {
