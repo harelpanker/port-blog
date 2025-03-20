08fe0304d13fa7f19c2d91d9690f4db0313a0896
Harel Panker <harelpanker@gmail.com>
2025-03-20 10:39:14 +0100

    spacing

--- app/components/layout/section-read-more.tsx
+++ app/components/layout/section-read-more.tsx
@@ -7,7 +7,7 @@ export default function SectionReadMore({ array }: { array: Post[] }) {
 	return (
 		<Section>
 			<Container className='!max-w-7xl'>
-				<ul className='flex flex-col gap-x-6 lg:grid lg:grid-cols-3 lg:gap-y-16'>
+				<ul className='flex flex-col gap-x-6 gap-y-14 lg:grid lg:grid-cols-3 lg:gap-y-16'>
 					{array.map((post) => (
 						<PostCard key={post.slug} {...post} />
 					))}
